a98db1f4e0ca898c9da3cdbc8f45fa57df0e0f60
kimskkkkky <ses513a@gmail.com>
2023-06-21 17:02:05 +0900

    실습_B

--- src/main/java/com/example/study_servlets/commons/Common.java
+++ src/main/java/com/example/study_servlets/commons/Common.java
@@ -1,5 +1,11 @@
 package com.example.study_servlets.commons;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class Common {
@@ -7,4 +13,26 @@ public class Common {
         return UUID.randomUUID().toString();
     }   
     
+
+    public Statement getStatement(){
+            String url = "jdbc:mysql://192.168.0.41:3306/db_cars";
+            String user = "yojulab";
+            String password = "!yojulab*";
+
+        Statement statement = null;
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return statement;
+    }
+
+    public String getGeneratorID(){
+        Date date = new Date();  
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyyhh:mm:ss");  
+        String strDate = formatter.format(date);
+        return strDate;
+    }
 }
