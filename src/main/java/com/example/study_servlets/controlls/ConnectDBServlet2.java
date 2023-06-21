8059ef06ab974dcc0ff864534a34e70f1eaf6590
kimskkkkky <ses513a@gmail.com>
2023-06-21 15:47:58 +0900

    실습_SKY

--- src/main/java/com/example/study_servlets/controlls/ConnectDBServlet2.java
+++ src/main/java/com/example/study_servlets/controlls/ConnectDBServlet2.java
@@ -1,6 +1,7 @@
 package com.example.study_servlets.controlls;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -25,19 +26,52 @@ public class ConnectDBServlet2 extends HttpServlet {
 
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("DB연결 성공\n");
-            // - query Edit
             Statement statement = connection.createStatement();
-            String query = "SELECT * FROM factorys";
-            ResultSet resultSet = statement.executeQuery(query); // 하면 결과값이 return됨. ..??
-            while (resultSet.next()) {
-                // query의 table을 가져올건데, 나올 값이 끝날때까지 돌기. next는 자체내에서 뭉치를 던지는 동작을함. 레코드가 다음레코드로
-                // 넘어감.
-                System.out.println(
-                        resultSet.getString("COMPANY_ID") + " " + // getStting은 DB의 type을 따라가기때문에 getString
-                                resultSet.getString("COMPANY"));
-            }
 
-            // SELECT count(*) AS CNT FROM factorys;
+            // 클라이언트에 html 화면 제공
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>bootstrap_tables</title>\r\n" + //
+                    "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\">\r\n" + //
+                    "    <!-- <link rel=\"stylesheet\" href=\"../CSS/commons.css\"> -->\r\n" + //
+                    "</head>\r\n" + //
+                    "\r\n" + //
+                    "<body>\r\n" + //
+                    "   <div class = \"container\">\r\n" + //
+                    "    <table class=\"table table-bordered table-hover\">\r\n" + //
+                    "        <thead>\r\n" + //
+                    "            <tr>\r\n" + //
+                    "                <th>COMPANY_ID</th>\r\n" + //
+                    "                <th>COMPANY</th>\r\n" + //
+                    "            </tr>\r\n" + //
+                    "        </thead>\r\n" + //
+                    "        <tbody>\r\n" ;
+
+
+            String query = "SELECT * FROM factorys";
+            ResultSet resultSet = statement.executeQuery(query); 
+            while (resultSet.next()) {
+                    contents = contents + "<tr>\r\n" + //
+                                          "<td>"+ resultSet.getString("COMPANY_ID")+"</td>\r\n" + //
+                                          "<td>"+ resultSet.getString("COMPANY") +"</td>\r\n" + //
+                                          "</tr>\r\n" ;
+            }
+                    contents = contents + "</tbody>\r\n" + //
+                                          "</table>\r\n" + //
+                                          "</div> \r\n" + //
+                                          "</body>\r\n" + //
+                                          "</html>";
+
+            // 클라이언트에 html 화면 제공
+            PrintWriter printWriter = response.getWriter(); 
+            printWriter.println(contents); 
+            printWriter.close();
+
             query = "SELECT count(*) AS CNT FROM factorys";
             resultSet = statement.executeQuery(query);
             int totalcount = 0;
@@ -45,26 +79,15 @@ public class ConnectDBServlet2 extends HttpServlet {
                 System.out.println(resultSet.getInt("CNT"));
                 totalcount = resultSet.getInt("CNT");
             }
-            // INSERT INTO factorys
-            // (COMPANY_ID,COMPANY)
-            // value
-            // ('CAR-01', 'AUDI');
 
             String COMPANY_ID = "CAR-01";
             String COMPANY = "AUDI";
-            // query ="INSERT INTO factorys " + "(COMPANY_ID,COMPANY) " + "value " + "('"+COMPANY_ID+"', '"+COMPANY+"') ";
-            // // 변수를 넣어줄때 싱글 쿼테이션 '' 안에 더블 쿼테이션 ""을 넣고 ++사이에 변수를 넣어줘야한다.
-            // int count = statement.executeUpdate(query);
 
-// UPDATE factorys
-// SET COMPANY = '패러리'
-// WHERE COMPANY_ID = 'CAR-01'
             COMPANY_ID = "CAR-01";
             COMPANY = "페라리";
             query ="UPDATE factorys " + "SET COMPANY ='"+COMPANY+"' " + "WHERE COMPANY_ID ='"+COMPANY_ID+"' ";  // CAR-01 AUDI를 페라리로 업데이트
 
-// DELETE FROM factorys
-// WHERE COMPANY_ID = 'CAR-01';
+
             query = "DELETE FROM factorys " + "WHERE COMPANY_ID = '"+COMPANY_ID+"'";  // CAR-01 페라리 삭제
             
             int count = statement.executeUpdate(query);
